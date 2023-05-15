1a4e3e7ee28d2a2e9cf5baff85c6f3db6255ce41
CamiloCorredor <camilocorredorroa@gmail.com>
2023-05-15 16:50:25 -0500

    DXF2

---
+++ ETL_DXF2/ETL_DXF2.py
@@ -0,0 +1,23 @@
+import geopandas as gpd
+import os
+
+path = 'LayersDXF/'
+files = os.listdir(path)
+
+# Create an empty GeoDataFrame to store all layers
+all_layers = gpd.GeoDataFrame()
+
+for file in files:
+    if file.endswith('.dxf'):
+        print(file)
+        layers = gpd.read_file(path + file, driver='DXF')
+        for layer in layers['Layer'].unique():
+            layer_df = layers[layers['Layer'] == layer]
+            layer_df = layer_df.set_crs("EPSG:3115")
+            # Append the current layer to the all_layers GeoDataFrame
+            all_layers = all_layers.append(layer_df)
+
+# Save the all_layers GeoDataFrame to a single GeoPackage file
+all_layers.to_file('all_layers.gpkg', driver='GPKG')
+
+print('Ejecución Finalizada')
